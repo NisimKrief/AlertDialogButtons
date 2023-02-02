22ea4fc73144856a81ea7fbe7e339ce130dd7177
NisimKrief <nk0787@bs.amalnet.k12.il>
2023-02-02 18:42:12 +0200

    Fixed

--- app/src/main/java/com/example/alertdialogbuttons/MainActivity2.java
+++ app/src/main/java/com/example/alertdialogbuttons/MainActivity2.java
@@ -2,16 +2,24 @@ package com.example.alertdialogbuttons;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Credits
  */
 public class MainActivity2 extends AppCompatActivity {
+    Intent gi = getIntent();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+
+    public void Finish(View view) {
+        finish();
     }
 }
